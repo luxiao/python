ad552b8157146d8281a3885241f1fb93f4b6cb87
卢肖 <luxiao1223@gmail.com>
2014-06-26 17:23:11 +0800

    Update Joke2to3.py

--- Joke2to3.py
+++ Joke2to3.py
@@ -1,5 +1,7 @@
 import urllib.request, pickle,os
 
+url = 'https://raw.githubusercontent.com/luxiao/python/master/youknowme.pkl'
+
 lines = opener.open(url).readlines()
 data = pickle.loads( b''.join(lines) )
 
