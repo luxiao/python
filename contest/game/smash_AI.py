ca22f574c8c22dbfeef90654b06beac5eb9ada1d
卢肖 <luxiao1223@gmail.com>
2014-10-15 16:35:35 +0800

    Update smash_AI.py

--- contest/game/smash_AI.py
+++ contest/game/smash_AI.py
@@ -33,43 +33,53 @@ def compare(src,target):
 
 def smash():
     target = gen()
-    #print 'target is: %s' % target
+    print 'target is: %s' % target
     init = splitList()
     result = {}
     flag = False
+    count = 0
     for ls in init[:-1]:             
-        result[ls] = compare(ls,target)
-        if sum([x+y for x,y in result.values()]) == step:
+        x,y = compare(ls,target)
+        count += 1
+        result[ls] = (x,y)
+        if x == step:
+            return count #直接在[1,2,3,4]或者[5,6,7,8]中找到了
+        elif sum([x+y for x,y in result.values()]) == step:
             flag = True
             break   
     if not flag:
         result[(9,)]=(0,1)
     #首轮判断完毕以后，消除value=(0,0)的子列表
     tmp = []
-    for x in result:
+    for x in result.keys():
         e,w = result[x]
         if e + w == 0:
-            tmp.append(x)
-    for i in tmp:
-        del result[i]
-    #print result
-    count = 2 #count记录猜测次数，由于前一轮判断已经匹配过两次，所以初始值为2
+            result.pop(x)
+    print result
     candy = []
     for i in result:
         candy = merge(candy,guess(i,result[i]))
     tmp=[]
     for c in candy:
         tmp.append(dict2list(c))
+    #print tmp
     assert(target in tmp)#这里假定结果一定在备选列表里，如果不在程序肯定猜不出来
     find = False
-    while not find and candy:
+    while not find:
         tmp = dict2list(candy.pop(0))
+        print tmp
         e,w = compare(tmp,target)
+        result[tuple(tmp)]=(e,w)
         count += 1
-        if e == 4:
+        if e == step:
             #print 'after %d times match, we\'ve found it!' % count
             #print tmp
             find = True
+        elif e == 0 and w != 0:            
+            for t in tmp:
+                for c in candy:
+                    if t in c.keys() and c[t] == tmp.index(t):
+                        candy.remove(c)
         else:
             for c in candy:
                 if compare(tmp,dict2list(c)) != (e,w):
@@ -146,7 +156,10 @@ def merge(als,bls):
 
 if __name__ == '__main__':
     counter = []
-    for i in range(10000):
+    number = 2
+    for i in range(number):
         counter.append(smash())
-    print sum(counter)//len(counter)
+    print '%d里平均猜中所需次数：%d，超过10次的有：%d' % (number,sum(counter)//len(counter),len([x for x in counter if x >10]))
+    
+
     
