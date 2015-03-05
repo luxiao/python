225439624e297dc52dc17d08e4685a9c309bc210
卢肖 <luxiao@users.noreply.github.com>
2015-03-05 15:50:12 +0800

    增加两个使用情景

--- decorator/easyd.py
+++ decorator/easyd.py
@@ -1,18 +1,28 @@
-def enhanced(meth):
-    def new(self, y):
-        print "I am enhanced"
-        return meth(self, y)
-    return new
-"""
-class C:
-    def bar(self, x):
-        print "some method says:", x
-    bar = enhanced(bar)"""
-    
-class C:
-    @classmethod
-    def foo(cls, y):
-        print "classmethod", cls, y
-    @enhanced
-    def bar(self, x):
-        print "some method says:", x
+def try_decorate(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        return f(*args, **kwargs)
+    return decorated
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = session.get(c.auth, None)
+        if auth:
+            return f(*args, **kwargs)
+        else:
+            return authenticate()
+    return decorated
+
+def db_handle(f):
+    @wraps(f)
+    def handleException(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except py.errors.DuplicateKeyError as e:
+            return dict({c.stat: c.s_dk})
+        except py.errors.AutoReconnect:
+            return dict({c.stat: 'mongodb replicaset down'})
+        except: 
+            return dict({c.stat: c.s_no})
+    return handleException
