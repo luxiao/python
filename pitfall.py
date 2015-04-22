336820d4683afd046e9e307966f63482b3290994
卢肖 <luxiao@users.noreply.github.com>
2015-04-22 18:03:09 +0800

    Update pitfall.py

--- pitfall.py
+++ pitfall.py
@@ -1,3 +1,6 @@
 最让人感到沮丧的错误：
 1、迭代list的时候，对list进行pop(i)操作。dict的pop则无影响。
+比如：for index, item in enumerate(list):
+          if item%2 == 0:
+              list.pop(index)
 改进：list = ［item for item in list if item%2 == 0]
